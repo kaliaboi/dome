dc9a384400728b9a43a8ff72255c8f02147f452f
designdevhybrid <abhishekkalia.productdesign@gmail.com>
2023-09-08 09:32:14 -0400

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,48 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, DM_Mono, DM_Sans } from "next/font/google";
+import localFont from "next/font/local";
 
 const inter = Inter({ subsets: ["latin"] });
 export const sans = DM_Sans({ subsets: ["latin"] });
 export const mono = DM_Mono({
   subsets: ["latin"],
   weight: ["300", "400", "500"],
+});
+
+const semi = localFont({
+  src: [
+    {
+      path: "../fonts/Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Medium.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/RegularItalic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/MediumItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../fonts/BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -21,7 +57,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={mono.className}>{children}</body>
+      <body className={semi.className}>{children}</body>
     </html>
   );
 }
