876bd4fd4441e1d7fd3c8f4e3864b14fb208ac5c
designdevhybrid <abhishekkalia.productdesign@gmail.com>
2023-09-21 22:25:29 -0400

    change api version

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@ export const sanityConfig = defineConfig({
   projectId: "4fanl4tz",
   dataset: "production",
   title: "Dome Studio",
-  apiVersion: "2023-09-01",
+  apiVersion: "2023-09-21",
   basePath: "/admin",
   plugins: [deskTool()],
   schema: { types: schemas },
